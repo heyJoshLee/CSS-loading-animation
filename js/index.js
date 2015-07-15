b2f5fbe31d303505faa6f44680f006ec95540099
Josh Lee <Lee45276@gmail.com>
2015-07-15 09:15:17 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$("#button-two").on("click", function() {
+  $(".ball").css({
+     "width": "20px",
+     "height" : "40px"
+   });
+});
+
+$("#button-one").on("click", function() {
+  $(".ball").css({
+     "width": "10px",
+     "height" : "40px"
+   });
+});
+
+$("#button-three").on("click", function() {
+  $(".ball").css({
+    "width": "5px",
+    "height" : "40px"
+   });
+});
+
+$("#button-four").on("click", function() {
+  $(".ball").css({
+    "width": "1px",
+    "height" : "140px"
+   });
+});
+
+$("#button-five").on("click", function() {
+  $(".ball").css({
+    "width": "30px",
+    "height" : "30px"
+   })
+});
